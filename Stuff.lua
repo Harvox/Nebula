9553e3554066816b296af32f88b40401c5801136
Harvox <134923684+Harvox@users.noreply.github.com>
2025-03-19 09:33:47 -0400

    Update Stuff.lua

--- Stuff.lua
+++ Stuff.lua
@@ -1,10 +1,11 @@
 return {
 ["UI"] = {
-    [1] = "xz",
-    [2] = "Vault",
-    [3] = "encode",
+    [1] = "UI: xz",
+    [2] = "UI: Vault",
+    [3] = "UI: encode",
+    [3] = "Contributor: Joyce",
 },
 ["Script"] = {
-  [1] = "Harvox",
+  [1] = "Script: Harvox",
 },
 }
